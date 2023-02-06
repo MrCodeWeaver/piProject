6eb6a0e4fc42594fd26a236509b42a7cc614bc0c
Nate <56313601+bcwalnja@users.noreply.github.com>
2023-02-06 13:57:15 -0500

    suggestion

--- Program.cs
+++ Program.cs
@@ -3,43 +3,29 @@ using System.Device.Gpio;
 using System.Threading;
 
 Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
-int pin = 17;
+int upPin = 17;
 int downPin = 27;
+int sleepTime = 4000;
 using var controller = new GpioController();
-controller.OpenPin(pin, PinMode.Output);
+controller.OpenPin(upPin, PinMode.Output);
 controller.OpenPin(downPin, PinMode.Output);
 
-//bool ledOn = true;
-//bool downOn = true;
 while (true)
 {
-    controller.Write(downPin, PinValue.Low);
-    Thread.Sleep(250);
-    controller.Write(pin, PinValue.Low);
+    TogglePinOnFor(upPin, sleepTime);
+    Thread.Sleep(sleepTime);
+    TogglePinOnFor(downPin, sleepTime);
+    Thread.Sleep(sleepTime);
+}
+
+void TogglePinOnFor(int pin, int milliseconds)
+{
+    if (controller is null)
+    {
+        return;
+    }
     
-    Thread.Sleep(2500);
-
-    controller.Write(downPin,  PinValue.Low);
-    Thread.Sleep(250);
     controller.Write(pin, PinValue.High);
-
-    Thread.Sleep(5000);
-
-    controller.Write(downPin, PinValue.Low);
-    Thread.Sleep(250);
+    Thread.Sleep(milliseconds);
     controller.Write(pin, PinValue.Low);
-
-    Thread.Sleep(3000);
-
-    controller.Write(downPin,  PinValue.High);
-    Thread.Sleep(250);
-    controller.Write(pin, PinValue.Low);
-
-    Thread.Sleep(5000);
-
-    controller.Write(downPin,  PinValue.Low);
-    Thread.Sleep(250);
-    controller.Write(pin, PinValue.Low);
-
-    Thread.Sleep(2500);
 }
