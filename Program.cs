aa06a62aa9b865275c85efc725d47c0c2466c8ba
Ryan Weaver <12052413+MrCodeWeaver@users.noreply.github.com>
2023-02-06 13:45:43 -0500

    broken code test

--- Program.cs
+++ Program.cs
@@ -3,13 +3,43 @@ using System.Device.Gpio;
 using System.Threading;
 
 Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
-int pin = 18;
+int pin = 17;
+int downPin = 27;
 using var controller = new GpioController();
 controller.OpenPin(pin, PinMode.Output);
-bool ledOn = true;
+controller.OpenPin(downPin, PinMode.Output);
+
+//bool ledOn = true;
+//bool downOn = true;
 while (true)
 {
-    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    controller.Write(downPin, PinValue.Low);
+    Thread.Sleep(250);
+    controller.Write(pin, PinValue.Low);
+    
+    Thread.Sleep(2500);
+
+    controller.Write(downPin,  PinValue.Low);
+    Thread.Sleep(250);
+    controller.Write(pin, PinValue.High);
+
     Thread.Sleep(5000);
-    ledOn = !ledOn;
+
+    controller.Write(downPin, PinValue.Low);
+    Thread.Sleep(250);
+    controller.Write(pin, PinValue.Low);
+
+    Thread.Sleep(3000);
+
+    controller.Write(downPin,  PinValue.High);
+    Thread.Sleep(250);
+    controller.Write(pin, PinValue.Low);
+
+    Thread.Sleep(5000);
+
+    controller.Write(downPin,  PinValue.Low);
+    Thread.Sleep(250);
+    controller.Write(pin, PinValue.Low);
+
+    Thread.Sleep(2500);
 }
