c765508f58b370286afd37179dbdc6293e1e9c08
Ryan Weaver <12052413+MrCodeWeaver@users.noreply.github.com>
2023-02-06 07:44:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+int pin = 18;
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    Thread.Sleep(5000);
+    ledOn = !ledOn;
+}
